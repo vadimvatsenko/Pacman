bc1fbcceccd482ce254a40cf4e0b0d8dd36e9424
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2023-12-24 11:26:16 +0200

    add Scene Sutup, LayerMask & Collision Matrix, Maze Pellets Nodes TileMaps, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Ghosts[] ghosts;
+    [SerializeField] Pacman pacman;
+    [SerializeField] Transform pellets;
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetScore(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhostEaten(Ghosts ghosts)
+    {
+        SetScore(this.score + ghosts.points);
+    }
+
+    public void PacmanEaten()
+    {
+        this.pacman.gameObject.SetActive(false);
+        SetLives(this.lives - 1);
+        if (this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f); // c задержкой в 3сек
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/Ghosts.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Ghosts : MonoBehaviour
+{
+    public int points = 200;
+}
