7efd1dd8b1d4f3c1e40e7c929e3dae0057f58e2d
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2023-12-29 14:13:07 +0200

    add Ghost Scarret Behaivor

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GhostScatter : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnDisable()
     {
-
+        this.ghost.chase.Enable();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Node node = other.GetComponent<Node>();
 
+        if (node != null && !this.enabled && !this.ghost.frightened.enabled)
+        {
+            int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+            {
+                index++;
+
+                if (index >= node.availableDirections.Count)
+                {
+                    index = 0;
+                }
+            }
+
+            this.ghost.movement.SetDirection(node.availableDirections[index]);
+        }
     }
 }
